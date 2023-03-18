aa9fda14c226c2ef4e4909ae6f34d3afaf2c4f98
IulianMohonea <126501200+IulianMohonea@users.noreply.github.com>
2023-03-18 16:20:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,226 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+
+std::ifstream fin("proiect.txt");
+
+class Player {
+private:
+    const int x;
+    const int y;
+    int hp;
+
+    std:: string name;
+public:
+    Player(const int x_, int y_, int hp_, std::string name_) : x{x_}, y{y_}, hp{hp_}, name{name_} {
+        std:: cout<<name << "\n";
+    }
+    // const de initializare
+    int alive () {
+        return hp;
+    }
+    void hit (){
+        --hp;
+    }
+    friend std::ostream &operator<<(std::ostream &os, const Player &player) {
+        os << "x: " << player.x << " y: " << player.y << " hp: " << player.hp << " name: " << player.name << "\n";
+        return os;
+    }
+    // op<<
+
+    void getxy(int& cx, int& cy){
+        cx = x;
+        cy = y;
+    }
+    void hpshow(){
+        std:: cout <<"Viata ramasa:" << hp<<"\n";
+    }
+
+};
+
+class Level {
+private:
+    int lvl;
+public:
+
+    Level(const int lvl_) : lvl{lvl_} {
+        std:: cout<<"Level initial: "<< lvl<< "\n";
+    }
+    // constructor de initializare
+
+    friend std::ostream &operator<<(std::ostream &os, const Level &level) {
+        os << "lvl: " << level.lvl;
+        return os;
+    }
+    void nextlvl(){
+        ++lvl;
+    }
+    void getlvl(int& clvl){
+        clvl = lvl;
+    }
+    // op<<
+};
+
+
+class Game {
+private:
+    static const int n = 20;
+    int m[n][n];
+    Player pl;
+    Level lv;
+
+public:
+    Game(Player player_ , Level level_) :pl{player_}, lv{level_}{
+        std:: cout<<pl << lv << '\n';
+    }
+    // const de initializare
+    void buildmatrix (){
+        for (int i = 0; i < n ; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                m[i][j] = 0;
+            }
+        }
+    }
+
+    void afmatrix (){
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        for (int i = 0; i < n ; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if(m[i][j] == -1){
+                    SetConsoleTextAttribute(hConsole,
+                                            BACKGROUND_RED);
+                    std:: cout<<" ";
+                }
+                else if (m[i][j] == 0){
+                    SetConsoleTextAttribute(hConsole,
+                                            BACKGROUND_GREEN);
+                    std:: cout<<" ";
+                }
+                else if(m[i][j] == 1){
+                    SetConsoleTextAttribute(hConsole,
+                                            BACKGROUND_BLUE);
+                    std:: cout<<" ";
+                }
+                else{
+                    SetConsoleTextAttribute(hConsole,
+                                            BACKGROUND_INTENSITY);
+                    std:: cout<<" ";
+                }
+
+            }
+            std:: cout<<"\n";
+            SetConsoleTextAttribute(hConsole,
+                                    BACKGROUND_GREEN);
+        }
+    }
+
+    void putinmatrix (int cx, int cy){
+        m[cx][cy] = 1;
+    }
+
+    void movement(int& cx, int& cy){
+        char mymove;
+
+        std::cin >> mymove;
+        if (mymove == 'W') {
+            cx--;
+        }
+        else if (mymove == 'A') {
+            cy--;
+        }
+        else if (mymove == 'S') {
+            cx++;
+        }
+        else if (mymove == 'D') {
+            cy++;
+        }
+        if(cx < 0) {cx = 0;}
+        else if(cx > 19) {cx = 19;}
+        if(cy < 0) {cy = 0;}
+        else if(cy > 19) {cy = 19;}
+    }
+
+    void randomshoot(int a, int b){
+
+        std:: cout<<a<<b<<"Fisier \n";
+        if (m[a][b] == 1)
+            m[a][b] = -2;
+        else
+            m[a][b] = -1;
+    }
+    int verhit (int cx, int cy){
+        if(m[cx][cy] == -2)
+            return 0;
+        return 1;
+    }
+    bool operator==(const Game &rhs) const {
+        std:: cout<<"Op=="<<"\n";
+        return n == rhs.n;
+    }
+    // op==
+
+    friend std::ostream &operator<<(std::ostream &os, const Game &game) {
+        os << "n: " << game.n;
+        std:: cout<< "game.n";
+        return os;
+    }
+    // op<<
+
+
+    ~Game() {
+        std:: cout<<"destructor Game" << "\n";
+    }
+    //destruct
+
+
+};
+void startgame()
+{
+
+    Player pl{10,10,3,"Chad"};
+    Level lv{0};
+    Game gm{pl,lv};
+    gm.buildmatrix();
+    int dummy = 0, dummy2, cx = 10, cy = 10;
+    while(pl.alive()){
+        pl.getxy(cx,cy);
+        lv.nextlvl();
+        lv.getlvl(dummy);
+        dummy2 = dummy;
+        std::cout << lv << "\n";
+        while(dummy != 0) {
+            gm.movement(cx, cy);
+            dummy--;
+        }
+        gm.putinmatrix(cx,cy);
+        gm.afmatrix();
+        std:: cout<<"Miscarea mea \n";
+        while(dummy2 != 0){
+            int a,b;
+            std::cin >> a >> b;
+            gm.randomshoot(a,b);
+            dummy2--;
+        }
+        gm.afmatrix();
+        std::cout<<"Random misc \n";
+        if(gm.verhit(cx,cy) == 0){
+            pl.hit();
+        }
+        pl.hpshow();
+        gm.buildmatrix();
+    }
+
+}
+
 
 int main() {
-    std::cout << "Hello, world!\n";
+
+    startgame();
+    std:: cout<< "Final de joc";
     return 0;
 }
